97dcf7b3bf8aa08ab4d45935bba0e2f4aff87860
Michelle <clairsin@gmail.com>
2022-06-30 10:16:52 -0400

    commit tree ad06541c3c24b2fe99eda472b160c3bc71cd4cc4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+
+  def index
+    birds = Bird.all
+    render json: birds
+  end 
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: bird
+    else 
+      render json: {error: 'no bird found'}, status: :not_found
+    end
+    
+  end
+  
 end
